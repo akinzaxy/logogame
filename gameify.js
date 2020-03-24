a98573a11810891be8b27af6be1ddc0777e4b26f
akinzaXY <lazers@technologist.com>
2020-03-24 12:52:50 +0100

    class logotype established with example

--- gameify.js
+++ gameify.js
@@ -3,3 +3,21 @@ var logo;
 var eng, lautechbread, passa, nuesa, fpas, fet, cse, mee, eee;
 
 // var entered = 
+// database of logos
+// logo
+class logoType {
+    constructor(str, addr) {
+        this.str = str;
+        this.addr = addr;
+    }
+}
+    // establishing example logos
+    cse = new logoType(cse, 'cse.png');
+    // matchable string
+// does entered match datbase
+    // scramble string
+    // present string
+    // add extra?
+// it does you won
+    // check matching
+    // alert win
